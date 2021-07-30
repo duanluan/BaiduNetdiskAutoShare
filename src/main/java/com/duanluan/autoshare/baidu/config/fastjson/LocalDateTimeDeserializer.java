06b055d400412e7d6c9ca183bc9cc01df17af2a0
duanluan <duanluan@outlook.com>
2021-07-30 15:27:42 +0800

    fix: 优化 FastJSON 反序列化逻辑，避免 Integer 时间戳转 LocalDateTime 时，时间戳值不正确导致转换失败而报错

--- src/main/java/com/duanluan/autoshare/baidu/config/fastjson/LocalDateTimeDeserializer.java
+++ src/main/java/com/duanluan/autoshare/baidu/config/fastjson/LocalDateTimeDeserializer.java
@@ -15,6 +15,15 @@ import java.time.LocalDateTime;
  */
 public class LocalDateTimeDeserializer implements ObjectDeserializer {
 
+  /**
+   * 反序列化
+   *
+   * @param parser    解析器，可以获取需要解析的值
+   * @param type      反序列化后的类型
+   * @param fieldName
+   * @param <T>       反序列化后的类型类
+   * @return 反序列化后的值
+   */
   @Override
   public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
     Object valObj = parser.parse();
@@ -22,10 +31,13 @@ public class LocalDateTimeDeserializer implements ObjectDeserializer {
     if (valObj != null && LocalDateTime.class.getName().equals(type.getTypeName())) {
       // 时间戳
       if (valObj instanceof Integer) {
-        Integer valInt = ((Integer) valObj).intValue();
-        if (valInt.toString().length() == 10) {
-          valObj = DateUtils.parseLocalDateTime(valInt);
+        long valInt = ((Integer) valObj).longValue();
+        // 10 位时间戳
+        if (Long.toString(valInt).length() == 10) {
+          valObj = DateUtils.parseLocalDateTime(valInt * 1000);
+          return (T) valObj;
         }
+        return null;
       }
     }
     return (T) valObj;
